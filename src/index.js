718def3962a20bd005de3920f1afed6ce09b71f0
MonkeyD-IchiJou <ichijou8282@gmail.com>
2017-12-15 12:21:15 +0800

    wrap the ReactDOM render into a function. For getting external variables

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+window.RenderApp = function (chatbotId, livechatId) {
+    console.log(chatbotId)
+    console.log(livechatId)
+    new ReactDOM.render(<App />, document.getElementById('root'));
+}
