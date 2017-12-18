f7b08da810abde8a8d0ab6f5574412cea760e7fc
MonkeyD-IchiJou <ichijou8282@gmail.com>
2017-12-18 11:29:24 +0800

    simple connection to socket server using my SocketConnect class

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,18 +1,54 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Button } from 'semantic-ui-react'
+import SocketConnect from './socketapi'
+import Chatbox from './components/Chatbox'
 
 class App extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            livechatSocket: new SocketConnect('livechatSocket')
+        }
+
+        let livechatSocket = this.state.livechatSocket
+        let livechatId = this.props.envReducer.livechatId
+
+        // if have livechatId...
+        // try to connect with my livechat socket server
+        livechatSocket.connectSocket(this.props.envReducer.backendUrl + '/lcIO')
+
+        // live chat socket subscribtions
+        livechatSocket.subscribe('connect', () => {
+
+            // asking to join room
+            livechatSocket.socketEmit('client_join_room', {
+                roomId: livechatId,
+                username: 'username',
+                message: 'message',
+                attentionLevel: 1
+            })
+
+            livechatSocket.subscribe('client_joined', (data) => {
+                //socketId = data.socketId
+                console.log(data.socketId)
+            })
+
+        })
+
+    }
+
+    componentWillUnmount() {
+        this.state.livechatSocket.disconnectSocket()
+    }
+
     render() {
-        console.log(this.props)
         return (
             <div>
-                this is my chatbox pls.. the other one is fake af
-                <Button>
-                    touch
-                </Button>
+                <Chatbox />
             </div>
-        );
+        )
     }
 }
 
