7477ee6081105628a172737b9ac3f82c48be4e31
MonkeyD-IchiJou <ichijou8282@gmail.com>
2017-12-18 12:19:39 +0800

    added more states for userReducer

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,9 +1,19 @@
 const userReducer = (
-    state = { username: '', email: '', socketId: '', userjoindate: '' },
+    state = { username: '', email: '', requireAttention: 1, problem: '' },
     action
 ) => {
 
     switch (action.type) {
+        case "SET_USER_INFO":
+            state = {
+                ...state,
+                username: action.payload.username,
+                email: action.payload.email,
+                requireAttention: action.payload.requireAttention,
+                problem: action.payload.problem
+            }
+            break
+
         default:
             break
     }
