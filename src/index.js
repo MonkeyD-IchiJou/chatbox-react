71d4507f17f28d8e2af0318839a70a3a47c50d49
MonkeyD-IchiJou <ichijou8282@gmail.com>
2017-12-18 10:06:57 +0800

    redux store implemented

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './containers/App'
+import store from './store'
+import { Provider } from 'react-redux'
 
 window.RenderApp = function (chatbotId, livechatId) {
+
     console.log(chatbotId)
     console.log(livechatId)
-    new ReactDOM.render(<App />, document.getElementById('root'));
+
+    new ReactDOM.render(
+        <Provider store={store}>
+            <App />
+        </Provider>,
+        document.getElementById('root')
+    )
 }
