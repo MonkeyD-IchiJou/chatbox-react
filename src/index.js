836ac14267b70cbbcddfd35c055a80dc72b9f401
MonkeyD-IchiJou <ichijou8282@gmail.com>
2017-12-18 11:38:25 +0800

    getting outside variables from react.. and then init them for envReducer

--- src/index.js
+++ src/index.js
@@ -3,16 +3,17 @@ import ReactDOM from 'react-dom'
 import App from './containers/App'
 import store from './store'
 import { Provider } from 'react-redux'
+import defaultEnvStore from './reducers/defaultEnvStore'
 import 'semantic-ui-css/semantic.min.css'
 
 window.RenderApp = function (chatbotId, livechatId) {
 
-    console.log(chatbotId)
-    console.log(livechatId)
+    defaultEnvStore.chatbotId = chatbotId
+    defaultEnvStore.livechatId = livechatId
 
     new ReactDOM.render(
         <Provider store={store}>
-            <App />
+            <App/>
         </Provider>,
         document.getElementById('root')
     )
