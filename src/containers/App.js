6d342e5aa9ce5be6796376398d42d0e3da814a5c
MonkeyD-IchiJou <ichijou8282@gmail.com>
2017-12-18 12:21:25 +0800

    must have user info before connect to live chat server

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import SocketConnect from './socketapi'
+import { setUser_act } from './actions/userActions'
 import Chatbox from './components/Chatbox'
 
 class App extends Component {
@@ -12,41 +13,65 @@ class App extends Component {
             livechatSocket: new SocketConnect('livechatSocket')
         }
 
-        let livechatSocket = this.state.livechatSocket
-        let livechatId = this.props.envReducer.livechatId
-
-        // if have livechatId...
-        // try to connect with my livechat socket server
-        livechatSocket.connectSocket(this.props.envReducer.backendUrl + '/lcIO')
-
-        // live chat socket subscribtions
-        livechatSocket.subscribe('connect', () => {
-
-            // asking to join room
-            livechatSocket.socketEmit('client_join_room', {
-                roomId: livechatId,
-                username: 'username',
-                message: 'message',
-                attentionLevel: 1
-            })
-
-            livechatSocket.subscribe('client_joined', (data) => {
-                //socketId = data.socketId
-                console.log(data.socketId)
-            })
-
-        })
+        // need to get the username and email first, before i can connect to live chat
+        this.props.dispatch(setUser_act('ichijou', 'ichijou8282@gmail.com', 1, 'some problem pls'))
 
     }
 
-    componentWillUnmount() {
+    connectToLivechatSocket = () => {
+
+        // disconnect the previous live chat if exist
+        this.disconnectLivechatSocket()
+
+        let envReducer = this.props.envReducer
+        let userReducer = this.props.userReducer
+
+        let livechatSocket = this.state.livechatSocket
+        let livechatId = envReducer.livechatId
+        let username = userReducer.username
+        let userproblem = userReducer.problem
+        let attentionLevel = userReducer.requireAttention
+
+        // need to have livechatId, username and userproblem.. then can connect to my server
+        if (livechatId && username && userproblem) {
+            // if have livechatId...
+            // try to connect with my livechat socket server
+            livechatSocket.connectSocket(envReducer.backendUrl + '/lcIO')
+
+            // live chat socket subscribtions
+            livechatSocket.subscribe('connect', () => {
+
+                // asking to join room
+                livechatSocket.socketEmit('client_join_room', {
+                    roomId: livechatId,
+                    username: username,
+                    message: userproblem,
+                    attentionLevel: attentionLevel
+                })
+
+                livechatSocket.subscribe('client_joined', (data) => {
+                    livechatSocket.setSocketId(data.socketId)
+                    console.log(livechatSocket.getSocketId())
+                })
+
+            })
+        }
+
+        
+    }
+
+    disconnectLivechatSocket = () => {
         this.state.livechatSocket.disconnectSocket()
+    }
+
+    componentWillUnmount() {
+        this.disconnectLivechatSocket()
     }
 
     render() {
         return (
             <div>
-                <Chatbox />
+                <Chatbox connectToLivechatSocket={this.connectToLivechatSocket}/>
             </div>
         )
     }
