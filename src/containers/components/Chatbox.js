f4c43e3c3826bb599fa86e89aa5ccfa6cf2c6f84
MonkeyD-IchiJou <ichijou8282@gmail.com>
2017-12-18 11:28:25 +0800

    temp components for ui purpose

---
+++ src/containers/components/Chatbox.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import { Button } from 'semantic-ui-react'
+
+class Chatbox extends Component {
+    render() {
+        return (
+            <div>
+                this is my chatbox pls.. the other one is fake af
+                <Button>
+                    touch
+                </Button>
+            </div>
+        );
+    }
+}
+
+export default Chatbox
