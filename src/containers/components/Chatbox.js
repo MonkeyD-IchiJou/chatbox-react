02030f9961b676723ffbc5d422ddb041f72ab577
MonkeyD-IchiJou <ichijou8282@gmail.com>
2017-12-19 14:21:04 +0800

    basic chatbox dumb component

--- src/containers/components/Chatbox.js
+++ src/containers/components/Chatbox.js
@@ -1,14 +1,48 @@
 import React, { Component } from 'react'
-import { Button } from 'semantic-ui-react'
+import ChatboxBody from './ChatboxBody'
+import ChatboxForm from './ChatboxForm'
+import { Accordion, Icon } from 'semantic-ui-react'
 
 class Chatbox extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            headerCollapse: true
+        }
+    }
+
+    handleHeaderClick = (e, titleProps) => {
+        let result = !this.state.headerCollapse
+        this.setState({ headerCollapse: result })
+    }
+
     render() {
         return (
-            <div>
-                this is my chatbox pls.. the other one is fake af
-                <Button onClick={() => { this.props.connectToLivechatSocket()}}>
-                    connecttolivechat
-                </Button>
+            <div style={{
+                width: '60%',
+                margin: '0 auto',
+                marginTop: '15px'
+            }}>
+                <Accordion fluid styled inverted={true}>
+
+                    <Accordion.Title
+                        active={this.state.headerCollapse}
+                        onClick={this.handleHeaderClick}
+                        style={{ backgroundColor: 'black' }}
+                    >
+                        Chatting with Matt
+                        <Icon name='dropdown' style={{ float: 'right' }} />
+                    </Accordion.Title>
+
+                    <Accordion.Content active={this.state.headerCollapse}>
+
+                        <ChatboxBody />
+                        <ChatboxForm />
+
+                    </Accordion.Content>
+
+                </Accordion>
             </div>
         )
     }
