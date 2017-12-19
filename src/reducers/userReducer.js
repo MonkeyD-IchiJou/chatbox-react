bca73f90010c6d4b6d3f98efaf7747c0e3bf1de8
MonkeyD-IchiJou <ichijou8282@gmail.com>
2017-12-19 14:20:08 +0800

    added additional states for userReducer. user is requesting a chatbot by default. chatbot is the priority unless user request for livechat. Or there is no chatbot, livechat will auto become the default

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,16 +1,44 @@
 const userReducer = (
-    state = { username: '', email: '', requireAttention: 1, problem: '' },
+    state = { 
+        username: '', 
+        email: '', 
+        requireAttention: 0, 
+        problem: '',
+        requestChatbot: true, 
+        requestLivechat: false 
+    },
     action
 ) => {
 
     switch (action.type) {
-        case "SET_USER_INFO":
+        case "SET_LIVECHAT_REQUIREMENT_FULFILLED":
             state = {
                 ...state,
                 username: action.payload.username,
-                email: action.payload.email,
-                requireAttention: action.payload.requireAttention,
                 problem: action.payload.problem
+            }
+            break
+
+        case "SET_ATTENTIONLEVEL":
+            state = {
+                ...state,
+                requireAttention: action.payload
+            }
+            break
+
+        case "SET_REQ_LIVECHAT":
+            state = {
+                ...state,
+                requestLivechat: true,
+                requestChatbot: false
+            }
+            break
+
+        case "SET_REQ_CHATBOT":
+            state = {
+                ...state,
+                requestChatbot: true,
+                requestLivechat: false
             }
             break
 
