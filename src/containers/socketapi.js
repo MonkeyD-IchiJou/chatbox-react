d9d26849afcc108e792992e819b37272a5606d6f
MonkeyD-IchiJou <ichijou8282@gmail.com>
2017-12-28 11:59:15 +0800

    only storing socketName and socket object

--- src/containers/socketapi.js
+++ src/containers/socketapi.js
@@ -3,39 +3,34 @@ import io from 'socket.io-client'
 class SocketConnect {
 
     constructor(socketName) {
-        this.socket = ''
         this.socketName = socketName
-        this.socketId = ''
+        this.socket = ''
     }
 
     connectSocket(url) {
+        // connect this socket to my socket server
         this.socket = io(url)
     }
 
     disconnectSocket() {
         if(this.socket) {
+            // close the socket
             this.socket.close()
         }
     }
 
     subscribe(channelName, callback) {
         if (this.socket) {
+            // subscribe for listening future event
             this.socket.on(channelName, callback)
         }
     }
 
     socketEmit(channelName, data) {
-        if (this.socket) { 
+        if (this.socket) {
+            // emit to a socket channel
             this.socket.emit(channelName, data)
         }
-    }
-
-    setSocketId(sid) {
-        this.socketId = sid
-    }
-
-    getSocketId() {
-        return this.socketId
     }
 
 }
