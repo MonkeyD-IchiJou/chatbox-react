5265a38d3ac0ca54ec9bac8b93f72a49e02758a7
MonkeyD-IchiJou <ichijou8282@gmail.com>
2018-01-08 17:20:42 +0800

    livechat form ui updated

--- src/containers/components/LivechatFormBody.js
+++ src/containers/components/LivechatFormBody.js
@@ -3,17 +3,37 @@ import { Form, Button } from 'semantic-ui-react'
 
 class LivechatFormBody extends Component {
 
+    constructor(props) {
+        super(props)
+        this.state = {
+            username: '',
+            email: '',
+            problem: '',
+            submittedusername: '',
+            submittedemail: '',
+            submittedproblem: '',
+        }
+    }
+
+    handleChange = (e, { name, value }) => this.setState({ [name]: value })
+
     handleSubmit = () => {
-        console.log('submit liao')
+        const { username, email, problem } = this.state
+        this.setState({
+            submittedusername: username,
+            submittedemail: email,
+            submittedproblem: problem
+        })
+        this.props.setUserInfo(username, email, problem)
     }
 
     render() {
         return (
             <Form onSubmit={this.handleSubmit}>
 
-                <Form.Input label='Username' placeholder='Username' />
-                <Form.Input label='Email' placeholder='Email' />
-                <Form.TextArea label='Message' placeholder='Tell us more about you...' />
+                <Form.Input name='username' label='Username' placeholder='Username' required onChange={this.handleChange}/>
+                <Form.Input name='email' label='Email' placeholder='Email' type='email' required onChange={this.handleChange}/>
+                <Form.TextArea name='problem' label='Message' placeholder='Tell us more about you...' required onChange={this.handleChange}/>
 
                 <Button type='submit'>Submit</Button>
 
