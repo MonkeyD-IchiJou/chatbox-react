d4d9fd615e8c6f388dddbd1aae2f84415b190b47
MonkeyD-IchiJou <ichijou8282@gmail.com>
2018-01-23 13:46:38 +0800

    only email

--- src/containers/components/ChatbotTmpForm.js
+++ src/containers/components/ChatbotTmpForm.js
@@ -6,21 +6,14 @@ class ChatbotTmpForm extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            username: '',
-            email: '',
-            submittedusername: '',
-            submittedemail: ''
+            email: ''
         }
     }
 
     handleChange = (e, { name, value }) => this.setState({ [name]: value })
 
     handleSubmit = () => {
-        const { username, email } = this.state
         this.setState({
-            submittedusername: username,
-            submittedemail: email,
-            username: '',
             email: ''
         })
         this.props.sendAcknowledgeMsg(this.props.indexToPop)
@@ -30,8 +23,7 @@ class ChatbotTmpForm extends Component {
         return (
             <Form onSubmit={this.handleSubmit}>
 
-                <Form.Input value={this.state.username} name='username' label='Username' placeholder='Username' required onChange={this.handleChange} />
-                <Form.Input value={this.state.email} name='email' label='Email' placeholder='Email' type='email' required onChange={this.handleChange} />
+                <Form.Input value={this.state.email} name='email' label='Agency Email' placeholder='Enter agency email address' type='email' required onChange={this.handleChange} />
 
                 <Button type='submit'>Submit</Button>
 
